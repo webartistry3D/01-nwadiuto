9f22ba6d088b6655dd023e64511d757c1ce382dc
WebArtistry.3D <webartistry.io@gmail.com>
2025-10-31 10:31:09 +0100

    modified: floatingwhatsapp.tsx

--- client/src/components/FloatingWhatsApp.tsx
+++ client/src/components/FloatingWhatsApp.tsx
@@ -1,11 +1,11 @@
 import { MessageCircle } from 'lucide-react';
 import { motion } from 'framer-motion';
 
-const whatsappNumber = '+2347017188070';
+const whatsappNumber = '+2347016361745';
 
 export default function FloatingWhatsApp() {
   const handleClick = () => {
-    const message = "Hi Style Plug, your collection is beautiful. I want to make an inquiry.";
+    const message = "Hi Nwadiuto, ❤️ your outfits! /nI would like to make some inquiries.";
     const whatsappUrl = `https://wa.me/${whatsappNumber}?text=${encodeURIComponent(message)}`;
     window.open(whatsappUrl, '_blank');
   };
